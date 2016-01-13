6096982d259ec490495b2efec85da8fe4cb412cd
chuanlu <orcuslc@hotmail.com>
2016-01-13 17:21:10 +0800

    rb_tree

--- CLRS/CLRS_with_Python/Learning/Chapter13/red_black_tree.py
+++ CLRS/CLRS_with_Python/Learning/Chapter13/red_black_tree.py
@@ -1,11 +1,25 @@
-class red_black_tree:
+red = 'red'
+black = 'black'
+
+
+class node:
 	def __init__(self):
+		self.color = None
+		self.parent = None
+		self.left = None
+		self.right = None
+		self.key = None
+
+class red_black_tree(node):
+	def __init__(self):
+		super().__init__()
 		self.nil = None
+		self.root = None
 
 	def left_rotate(self, x):
 		y = x.right
 		x.right = y.left
-		if y.left != self.nil:
+		if y.left != self.nil: 
 			y.left.p = x
 		y.p = x.p
 		if x.p == self.nil:
@@ -32,3 +46,53 @@ class red_black_tree:
 		x.right = y
 		y.p = x
 	
+	def insert(self, z):
+		y = self.nil
+		x = self.root
+		while x != self.nil:
+			y = x
+			if z.key < x.key:
+				x = x.left
+			else:
+				x = x.right
+		z.p = y
+		if y == self.nil:
+			self.root = z
+		elif z.key < y.key:
+			y.left = z
+		else:
+			y.right = z
+		z.left = self.nil
+		z.right = self.nil
+		z.color = red
+		self.insert_fixup(self, z)
+
+	def insert_fixup(self, z):
+		while z.p.color == red:
+			if z.p == z.p.p.left:
+				y = z.p.p.right
+				if y.color == red:
+					z.p.color = black
+					y.color = black
+					z.p.p.color = red
+					z = z.p.p
+				elif z == z.p.right:
+					z = z.p
+					self.left_rotate(z)
+				z.p.color = black
+				z.p.p.color = red
+				self.right_rotate(z.p.p)
+			else:
+				y = z.p.p.left
+				if y.color == red:
+					z.p.color = black
+					y.color = black
+					z.p.p.color = red
+					z = z.p.p
+				elif z == z.p.left:
+					z = z.p
+					self.right_rotate(z)
+				z.p.color = black
+				z.p.p.color = red
+				self.left_rotate(z)
+		self.root.color = black
