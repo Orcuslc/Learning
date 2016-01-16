af485f48a5ffd5b95999e5317c9baac7b41593e0
chuanlu <orcuslc@hotmail.com>
2016-01-17 00:03:50 +0800

    is_right_turn revised

--- Computational Geometry/convex_hull.py
+++ Computational Geometry/convex_hull.py
@@ -5,7 +5,8 @@ import random
 def convex_hull(P):
 	'''
 	input: a list of points P, points in which has a format of [x, y]
-	output: a list of points Q, which is the convex hull of P
+	output: two lists of points L_upper, L_lower, which are the 
+	upper and lower parts of the convex hull of P
 	'''
 	n = len(P)
 	if n <= 3:
@@ -30,7 +31,9 @@ def convex_hull(P):
 		check if three points make a right turn,
 		where the input points have a format of [x, y]
 		'''
-		if b[1] < c[1]:
+		line_ac = lambda x: (c[1] - a[1])/(c[0] - a[0]) * (x - a[0]) + a[1]
+		line_b = line_ac(b[0])
+		if line_b > b[1]:
 			return False
 		else:
 			return True
@@ -41,28 +44,27 @@ def convex_hull(P):
 		L_upper.append(P[i])
 		while len(L_upper) >= 3 and is_right_turn(L_upper[-1], L_upper[-2], L_upper[-3]) == False:
 			L_upper.pop(-2)
-			L_upper.pop(-2)
 	L_lower = [P[-1], P[-2]]
-	for i in range(n - 3, -1):
+	for i in range(n - 3, -1, -1):
 		L_lower.append(P[i])
 		while len(L_lower) >= 3 and is_right_turn(L_lower[-1], L_lower[-2], L_lower[-3]) == True:
 			L_lower.pop(-2)
-			L_lower.pop(-2)
-	L_lower.pop(0)
-	L_lower.pop(-1)
-	L_upper.extend(L_lower)
-	return L_upper
+
+	return L_upper, L_lower
 
 if __name__ == '__main__':
 	P_x = [random.random() * 10 for i in range(10)]
 	P_y = [random.random() * 10 for i in range(10)]
 	P = [[P_x[i], P_y[i]] for i in range(len(P_x))]
-	convex = convex_hull(P)
+	L_upper, L_lower = convex_hull(P)
 	P_x = [i[0] for i in P]
 	P_y = [i[1] for i in P]
-	convex_x = [i[0] for i in convex]
-	convex_y = [i[1] for i in convex]
+	lower_x = [i[0] for i in L_lower]
+	lower_y = [i[1] for i in L_lower]
+	upper_x = [i[0] for i in L_upper]
+	upper_y = [i[0] for i in L_upper]
 	plt.plot(P_x, P_y, 'r.')
-	plt.plot(convex_x, convex_y, 'b')
+	plt.plot(lower_x, lower_y, 'b')
+	plt.plot(upper_x, upper_y, 'b')
 	plt.show()
 
