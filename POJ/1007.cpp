9768d553205ae3ce1542c60c13bef2581485a08b
orcuslc <orcuslc@hotmail.com>
2017-03-27 08:09:35 +0800

    add 1007

---
+++ POJ/1007.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class str_rev {
+public:
+	string str;
+	int rev;
+private:
+	int count_rev();
+};
+
+int str_rev::count_rev() {
+	rev = 0;
+	
+}
+
+int main() {
+	int len, num;
+	cin >> len >> num;	
+	return(0);
+}
