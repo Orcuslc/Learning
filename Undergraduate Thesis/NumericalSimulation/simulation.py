5aef211407743b9e5235a9aa9d88b8ada110daf5
orcuslc <orcuslc@hotmail.com>
2017-05-16 01:06:09 +0800

    update

--- Undergraduate Thesis/NumericalSimulation/simulation.py
+++ Undergraduate Thesis/NumericalSimulation/simulation.py
@@ -92,15 +92,15 @@ def stat(data):
 	# Cov_u_b_im = np.asarray([cov(data[0][:, i]+1j*data[1][:, i], data[2][:, i]+1j*data[3][:, i])[1] for i in range(col)])
 	# Cov_u_b_star_re = np.asarray([cov(data[0][:, i]+1j*data[1][:, i], data[2][:, i]-1j*data[3][:, i])[0] for i in range(col)])
 	# Cov_u_b_star_im = np.asarray([cov(data[0][:, i]+1j*data[1][:, i], data[2][:, i]-1j*data[3][:, i])[1] for i in range(col)])
-	Cov_u_u_star = np.asarray([cov_u_u_star(data[0][:, i]+1.j*data[1][:, i]) for i in range(col)], dtype=np.complex64)
+	Cov_u_u_star = np.asarray([cov_u_u_star(data[0][:, i], data[1][:, i]) for i in range(col)], dtype=np.complex64)
 	Cov_u_gamma = np.asarray([cov_u_gamma(data[0][:, i]+1.j*data[1][:, i], data[4][:, i]) for i in range(col)], dtype=np.complex64)
 	Cov_u_b = np.asarray([cov_u_b(data[0][:, i]+1.j*data[1][:, i], data[2][:, i]+1.j*data[3][:, i]) for i in range(col)], dtype=np.complex64)
 	Cov_u_b_star = np.asarray([cov_u_b_star(data[0][:, i]+1.j*data[1][:, i], data[2][:, i]-1.j*data[3][:, i]) for i in range(col)], dtype=np.complex64)
 	return mean, [var[0]+var[1], var[2]+var[3], var[4]], [sp.real(Cov_u_u_star), sp.imag(Cov_u_u_star), sp.real(Cov_u_gamma), sp.imag(Cov_u_gamma), sp.real(Cov_u_b), sp.imag(Cov_u_b), sp.real(Cov_u_b_star), sp.imag(Cov_u_b_star)]
 
-def cov_u_u_star(u):
-	# return np.var(x)-np.var(y)+2j*(np.cov(x, y)[0, 0])
-	return np.mean(u**2) - (np.mean(u))**2
+def cov_u_u_star(x, y):
+	return np.var(x)-np.var(y)+2j*(np.cov(x, y)[0, 0])
+	# return np.mean(u**2) - (np.mean(u))**2
 
 def cov_u_gamma(u, gamma):
 	return np.mean(u*gamma) - np.mean(u)*np.mean(gamma)
