380bc9b62d1e74f66852025022069703d6b1fbe0
orcuslc <orcuslc@hotmail.com>
2017-05-21 00:38:12 +0800

    add 21

---
+++ LeetCode/21. Merge Two Sorted Lists.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        ListNode n(INT_MIN);
+        ListNode* head = &n;
+        while(l1 && l2) {
+            if(l1->val < l2->val) {
+                head->next = l1;
+                l1 = l1->next;
+            }
+            else {
+                head->next = l2;
+                l2 = l2->next;
+            }
+            head = head->next;
+        }
+        head->next = l1?l1:l2;
+        return n.next;
+    }
+};
