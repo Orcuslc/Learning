aea79e6b7a99132b87471dfdacaf037eaafc64be
orcuslc <orcuslc@hotmail.com>
2017-06-22 20:56:11 +0800

    add leetcode 167

---
+++ LeetCode/167. Two Sum II - Input array is sorted.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int i = 0, j = numbers.size()-1;
+        vector<int> res;
+        while(i < j) {
+            int sum = numbers[i] + numbers[j];
+            if(sum == target) {
+                res.push_back(i+1);
+                res.push_back(j+1);
+                return res;
+            }
+            else if(sum < target) i++;
+            else j--;
+        }
+        return res;
+    }
+};
