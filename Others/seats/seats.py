89ebf153be3223a83b85297f6f5bc10bfb5bc402
orcuslc <orcuslc@hotmail.com>
2017-09-17 19:54:38 -0500

    update

--- Others/seats/seats.py
+++ Others/seats/seats.py
@@ -3,8 +3,8 @@ import csv
 from O365 import Message
 
 ###### configuration ######
-loginid = ********
-password = *******
+loginid = 
+password = 
 authenticiation = (loginid, password)
 ###########################
 
@@ -13,8 +13,12 @@ path = sys.argv[1]
 def _parse(row):
 	[name, hawkid, seat] = row
 	email = hawkid + "@iowa.uiowa.edu"
-	title = "MATH:1380 Mid Term 1 Seat: " + seat
-	content = "Dear " + name + ":\n" + "Your seat for MATH:1380 Mid Term 1 is: " + seat + "\n" + "PLEASE IGNORE THE LAST EMAIL ABOUT YOUR MID TERM 1 SEAT IF YOU RECEIVED ONE, SINCE THIS IS AN UPDATED VERSION. Thank you!"
+	seat = seat.split(' ')
+	seat.remove('')
+	seat[0], seat[1] = seat[1], seat[0]
+	title = "MATH:1380 Mid Term 1 Seat: " + '-'.join(seat)
+	content = "Dear " + name + ":\n\n" + "Your seat for MATH:1380 Mid Term 1 is: " + "Building " + seat[0] + ", Room " + seat[1] + ", and Seat " + seat[2] + ". It is recommended that you explore your seat a day before the exam."
+	 # + "PLEASE IGNORE THE LAST EMAIL ABOUT YOUR MID TERM 1 SEAT IF YOU RECEIVED ONE, SINCE THIS IS AN UPDATED VERSION. Thank you!"
 	return [name, email, title, content]
 
 def send(msg, name, email, title, content):
