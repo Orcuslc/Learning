876894a3722ccaa9c944b39b96a98b9a79f2d239
orcuslc <orcuslc@hotmail.com>
2017-11-12 15:30:57 -0600

    update

--- TA/Grade/read.py
+++ TA/Grade/read.py
@@ -1,21 +1,45 @@
 import re
-pattern = re.compile(r"Testing Services Student Score Report.*?335-0356", re.S)
+import random
 
-def parse(content):
-	group = re.findall(pattern, content)
+def split(content):
+	pattern = re.compile(r"Testing Services Student Score Report.*?335-0356", re.S)
+	pages = re.findall(pattern, content)
+	return pages
 
-	return group[1]
-
-def parse_group(s):
+def parse_page(page):
 	namepattern = re.compile(r"^Testing.*$", re.M)
-	nameline = re.findall(namepattern, s)
+	nameline = re.findall(namepattern, page)
 	name = nameline[0][38:]
-	ID = re.findall()
+	ID = re.findall(r"[0-9]{8}", page)[0]
+	grade = re.findall(r"[0-9].*\.[0-9]{4}%", page)[0]
+	errorpattern = re.compile(r"Format.*?If allowed", re.S)
+	errors = re.findall(errorpattern, page)[0][57:-14].split('\n\n')
+	return [name, ID, grade, errors]
 
-if __name__ == '__main__':
+def parse(path):
+	with open(path) as f:
+		content = f.read()
+	f.close()
+	pagelist = split(content)
+	for page in pagelist:
+		[name, ID, grade, errors] = parse_page(page)
+		if len(errors) < 3: # No error
+			errors = "Wow! No errors!"
+		else:
+			errors = " ".join(errors)
+		print("Name:" + name, "ID:" + ID, "grade:" + grade, "errors:" + errors)
+
+
+def test():
+	path = "test.txt"
 	with open("test.txt") as f:
 		content = f.read()
 	f.close()
-	s = parse(content)
-	parse_group(s)
-	# print(s)
+	pages = split(content)
+	[name, ID, grade, errors] = parse_page(pages[random.randint(0, len(pages)-1)])
+	errors = " ".join(errors)
+	print("Name:" + name, "ID:" + ID, "grade:" + grade, "errors:" + errors)
+
+if __name__ == '__main__':
+	# test()
+	parse('test.txt')
