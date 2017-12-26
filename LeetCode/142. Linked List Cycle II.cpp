8e3cb5c91f745aa08bca2521870cf644766e604d
Chuan <orcuslc@hotmail.com>
2017-12-25 20:35:39 -0600

    update

---
+++ LeetCode/142. Linked List Cycle II.cpp
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        ListNode* fast = head, *slow = head;
+        while(fast && fast->next) {
+            fast = fast->next->next;
+            slow = slow->next;
+            if(fast == slow) break;
+        }
+        if(!fast || !fast->next) return NULL;
+        // slow and head will meet at the real entry (according to the difference of distance);
+        while(slow != head) {
+            slow = slow->next;
+            head = head->next;
+        }
+        return head;
+    }
+};
