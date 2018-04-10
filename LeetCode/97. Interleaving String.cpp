0187a2cacd2c1e9ed62894853708d2df13342027
orcuslc <orcuslc@hotmail.com>
2018-04-10 11:31:39 -0500

    update

---
+++ LeetCode/97. Interleaving String.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isInterleave(string s1, string s2, string s3) {
+        if(s3.size() != s1.size()+s2.size()) {
+            return false;
+        }
+        // dp[i][j] means if s3[:i+j] is the interleaving of s1[:i] and s2[:j]
+        int dp[s1.size()+1][s2.size()+1] = {0};
+        for(int i = 0; i <= s1.size(); i++) {
+            for(int j = 0; j <= s2.size(); j++) {
+                if(i == 0 && j == 0) {
+                    dp[i][j] = 1;
+                }
+                else if(i == 0) {
+                    dp[i][j] = dp[i][j-1] && s2[j-1] == s3[j-1];
+                }
+                else if(j == 0) {
+                    dp[i][j] = dp[i-1][j] && s1[i-1] == s3[i-1];
+                }
+                else {
+                    dp[i][j] = (dp[i-1][j] && s1[i-1] == s3[i+j-1]) || (dp[i][j-1] && s2[j-1] == s3[i+j-1]);
+                }
+            }
+        }
+        return dp[s1.size()][s2.size()];
+    }
+};
